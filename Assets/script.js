f09972ca1a33324db59554595a7c0844988d9ecf
Vivid Quill <sjdoublec@gmail.com>
2023-07-07 17:20:54 -0500

    styled opening page of quiz app with css

---
+++ Assets/script.js
@@ -0,0 +1,9 @@
+var countdownTimer = document.querySelector(".timer-count").innerHTML;
+var clickToPlayButton = document.querySelector(".start-button");
+
+
+// console.log(countdownTimer);
+
+clickToPlayButton.addEventListener("click", function() {
+    
+})
