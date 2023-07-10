acacb5c9bee771e15f0b8c059e6d43d273e27ded
Vivid Quill <sjdoublec@gmail.com>
2023-07-10 11:25:57 -0500

    updated HTML structure and JavaScript of multiple choice quiz with a submit button

--- Assets/script.js
+++ Assets/script.js
@@ -1,54 +1,138 @@
-var quizH1 = document.querySelector("#questions").children[0]; // <li>Coding 101<li>
-var choiceList1 = document.querySelector("#questions").children[1]; //"What do you know about coding? Take this short quiz to find out!"
-var choiceList2 = document.querySelector("#questions").children[2]; //"For each wrong answer you will lose 10 of your 60 seconds."
-var choiceList3 = document.querySelector("#questions").children[3]; //"Answer all 3 multiple choice questions before time runs out!"
+var questions = [
+    { 
+        question: "What is the acronym for Hypertext Markup Language?",
+        a: "HTML",
+        b: "HML",
+        c: "HTMUL",    
+        correct: "HTML",
+    },
 
-// console.log(choiceList3);
+    {
+        question: "What is CSS mainly used to add to a webpage?",
+        a: "interactivity",
+        b: "style",
+        c: "structure",
+        correct: "style",
+    },
 
-var startButton = document.querySelector(".start-button");
+    {
+        question: "What year was JavaScript invented?",
+        a: "2015",
+        b: "2005",
+        c: "1995",
+        correct: "1995"
+    }
+];
 
-var secondsLabel = document.querySelector(".timer-count");
-var totalSeconds = 3;
-var timerInterval;
+var quiz = document.getElementById("quiz");
+var answerElements = document.querySelectorAll(".answer");
+var questionElement = document.getElementById("question"); // Question is loading
+var a_text = document.getElementById("a_text");
+var b_text = document.getElementById("b_text");
+var c_text = document.getElementById("c_text");
+var submitButton = document.getElementById("submit");
 
-// var questionArea = document.querySelector("#questions");
-    // "What do you know about coding? Take this short quiz to find out!"
-    // "For each wrong answer you will lose 10 of your 60 seconds."
-    // "Answer all 3 multiple choice questions before time runs out!"
+var currentQuiz = 0;
+var score = 0;
 
-var allQuestions = {"What is the acronym for Hypertext Markup Language?" : ["HTML", "HML", "HTMUL"]};
-    // "What is CSS mainly used to add to a webpage?" : ["interactivity", "style" , "structure", 1],
-    // "What year was JavaScript invented?" : ['2015', '2005', '1995', 2]
-// };
+function deselectAnswers() {
+    answerElements.forEach((answer) => (answer.checked = false));
+};
 
-var question1 = Object.keys(allQuestions); // "What is the acronym for Hypertext Markup Language?"
-var arrayOfAnswerValues = Object.values(allQuestions); // [Array(3)]
-var answers1List = arrayOfAnswerValues[0]; // ["HTML", "HML", "HTMUL"]
+// console.log(deselectAnswers());
 
-var answer1 = answers1List[0];
-var answer2 = answers1List[1];
-var answer3 = answers1List[2];
+function getSelected() {
+    var answer;
+    answerElements.forEach((answerElement) => {
+      if (answerElement.checked) answer = answerElement.id;
+    });
+    return answer;
+};
+
+// console.log(getSelected());
+
+function loadQuiz() {
+    deselectAnswers();
+    var currentQuizData = questions[currentQuiz];
+    questionElement.innerText = currentQuizData.questions;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+};
+  
+loadQuiz();
+
+submitButton.addEventListener("click", function() {
+    var answer = getSelected();
+    if (answer) {
+      if (answer === questions[currentQuiz].correct) score++;
+      currentQuiz++;
+      if (currentQuiz < questions.length) loadQuiz();
+      else {
+        quiz.innerHTML = `
+              <h2>You answered ${score}/${questions.length} questions correctly</h2>
+              <button onclick="history.go(0)">Play Again</button>
+          `
+      }
+    }
+  });
+
+  // startButton.addEventListener("click", function() {
+//     nextQuestion();
+//     // startQuiz();
+//     setTime();
+//     buttonDisappear();
+//     // loadQuestion();
+// });
+
+
+
+// function nextQuestion() {
+//     document.getElementById("title").textContent = questions[0].title;
+//     document.getElementById("choice1").textContent = questions[0].choices[0];
+//     document.getElementById("choice2").textContent = questions[0].choices[1];
+//     document.getElementById("choice3").textContent = questions[0].choices[2];
+// }
+
+// function checkForAnswer() {
+    
+// }
+
+
+// Timer code block.
+// var startButton = document.querySelector(".start-button");
+// var secondsLabel = document.querySelector(".timer-count");
+// var totalSeconds = 3;
+// var timerInterval;
+
+// var question1 = Object.keys(allQuestions); // "What is the acronym for Hypertext Markup Language?"
+// var arrayOfAnswerValues = Object.values(allQuestions); // [Array(3)]
+// var answers1List = arrayOfAnswerValues[0]; // ["HTML", "HML", "HTMUL"]
+
+// var answer1 = answers1List[0];
+// var answer2 = answers1List[1];
+// var answer3 = answers1List[2];
 
 
 // var currentQuestion = 0;
 
-function startQuiz() {
-    quizH1.textContent = question1;
-    quizH1.style.fontSize = "32px";
-    quizH1.style.paddingBottom = "50px";
+// function startQuiz() {
+//     quizH1.textContent = question1;
+//     quizH1.style.fontSize = "32px";
+//     quizH1.style.paddingBottom = "50px";
     
-    choiceList1.textContent = answer1
-    choiceList1.style.backgroundColor = "mediumpurple";
-    choiceList1.style.color = "#fff";
+//     choiceList1.textContent = answer1
+//     choiceList1.style.backgroundColor = "mediumpurple";
+//     choiceList1.style.color = "#fff";
     
-    choiceList2.textContent = answer2;
-    choiceList2.style.backgroundColor = "mediumpurple";
-    choiceList2.style.color = "#fff";
+//     choiceList2.textContent = answer2;
+//     choiceList2.style.backgroundColor = "mediumpurple";
+//     choiceList2.style.color = "#fff";
 
-    choiceList3.textContent = answer3;
-    choiceList3.style.backgroundColor = "mediumpurple";
-    choiceList3.style.color = "#fff";
-}
+//     choiceList3.textContent = answer3;
+//     choiceList3.style.backgroundColor = "mediumpurple";
+//     choiceList3.style.color = "#fff";
+// }
         // Select first child element:
         // var changeListItems = document.getElementById("questions").children[0];
 
@@ -71,26 +155,26 @@ function startQuiz() {
     // changeToQuestions.textContent = // Object with 3 questions and multiple choice answers.
 
 
-function setTime() {
-    timerInterval = setInterval(function() {
-        --totalSeconds;
-        secondsLabel.innerHTML = pad(totalSeconds%60);
-    }, 1000);
-}
+// function setTime() {
+//     timerInterval = setInterval(function() {
+//         --totalSeconds;
+//         secondsLabel.innerHTML = pad(totalSeconds%60);
+//     }, 1000);
+// }
 
 // function stopTimer() {
 //     clearInterval(timerInterval);
 //     alert("You have run out of time!")
 // } 
 
-function pad(val) {
-    var valString = val + "";
-    if (valString.length < 2) {
-        return 0 + valString;
-    } else {
-        return valString;
-    }
-}
+// function pad(val) {
+//     var valString = val + "";
+//     if (valString.length < 2) {
+//         return 0 + valString;
+//     } else {
+//         return valString;
+//     }
+// }
 
 // function loadQuestion(current) {
 //     var question = Object.keys(allQuestions)[current];
@@ -99,16 +183,17 @@ function pad(val) {
 //     questionArea.textContent = question;
 // }
 
-function buttonDisappear() {
-    startButton.setAttribute("style", "display:none");
-}
+// function buttonDisappear() {
+//     startButton.setAttribute("style", "display:none");
+// }
 
-startButton.addEventListener("click", function() {
-    startQuiz();
-    setTime();
-    buttonDisappear();
-    // loadQuestion();
-});
+// startButton.addEventListener("click", function() {
+//     nextQuestion();
+//     // startQuiz();
+//     setTime();
+//     buttonDisappear();
+//     // loadQuestion();
+// });
 
 // clickToStopButton.addEventListener("click", stopTimer);
 
@@ -148,3 +233,62 @@ startButton.addEventListener("click", function() {
 // node.appendChild(document.createTextNode('Scooter'));
  
 // document.querySelector('ul').appendChild(node);
+
+
+
+// Different style of question object.
+// var questionArea = document.querySelector("#questions");
+    // "What do you know about coding? Take this short quiz to find out!"
+    // "For each wrong answer you will lose 10 of your 60 seconds."
+    // "Answer all 3 multiple choice questions before time runs out!"
+
+//     var allQuestions = {"What is the acronym for Hypertext Markup Language?" : ["HTML", "HML", "HTMUL"]};
+//     // "What is CSS mainly used to add to a webpage?" : ["interactivity", "style" , "structure", 1],
+//     // "What year was JavaScript invented?" : ['2015', '2005', '1995', 2]
+// // };
+
+// var question1 = Object.keys(allQuestions); // "What is the acronym for Hypertext Markup Language?"
+// var arrayOfAnswerValues = Object.values(allQuestions); // [Array(3)]
+// var answers1List = arrayOfAnswerValues[0]; // ["HTML", "HML", "HTMUL"]
+
+
+// // Start of hardcoding answers to styling.
+// var answer1 = answers1List[0];
+// var answer2 = answers1List[1];
+// var answer3 = answers1List[2];
+
+
+// // var currentQuestion = 0;
+
+// function startQuiz() {
+//     quizH1.textContent = question1;
+//     quizH1.style.fontSize = "32px";
+//     quizH1.style.paddingBottom = "50px";
+    
+//     choiceList1.textContent = answer1
+//     choiceList1.style.backgroundColor = "mediumpurple";
+//     choiceList1.style.color = "#fff";
+    
+//     choiceList2.textContent = answer2;
+//     choiceList2.style.backgroundColor = "mediumpurple";
+//     choiceList2.style.color = "#fff";
+
+//     choiceList3.textContent = answer3;
+//     choiceList3.style.backgroundColor = "mediumpurple";
+//     choiceList3.style.color = "#fff";
+// }
+
+
+// Variables to access different parts of the HTML.
+
+// var quizH1 = document.querySelector("#questions").children[0]; // <li>Coding 101<li>
+// var choiceList1 = document.querySelector("#questions").children[1]; //"What do you know about coding? Take this short quiz to find out!"
+// var choiceList2 = document.querySelector("#questions").children[2]; //"For each wrong answer you will lose 10 of your 60 seconds."
+// var choiceList3 = document.querySelector("#questions").children[3]; //"Answer all 3 multiple choice questions before time runs out!"
+
+// // console.log(choiceList3);
+
+// // var questionArea = document.querySelector("#questions");
+//     // "What do you know about coding? Take this short quiz to find out!"
+//     // "For each wrong answer you will lose 10 of your 60 seconds."
+//     // "Answer all 3 multiple choice questions before time runs out!"
